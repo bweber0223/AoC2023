9d76d24d5b4167a5c6b550652f97ba8fc38e0340
bweber0223 <36210094+bweber0223@users.noreply.github.com>
2023-12-01 11:42:13 -0500

    Add files via upload
    Add parts 1 and 2 to directory

---
+++ Day One/part1.py
@@ -0,0 +1,10 @@
+from day1 import DayOne
+
+class PartOne(DayOne):
+    def parse(self, line, i):
+        if line[i].isdigit():
+            return int(line[i])
+        return -1
+    
+part1 = PartOne()
+part1.main()
