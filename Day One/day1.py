db90585a17ddcc6a31b6443c8804191aa321c0a6
bweber0223 <36210094+bweber0223@users.noreply.github.com>
2023-12-01 11:40:55 -0500

    Create day1.py in Day One directory

---
+++ Day One/day1.py
@@ -0,0 +1,39 @@
+from abc import ABC, abstractmethod
+
+class DayOne(ABC):
+    def __init__(self):
+        self.lines = []
+
+    def main(self):
+        filename = input("Input file: ")
+        self.get_lines(filename)
+        print(f"Answer: {self.process()}")
+
+    def process(self):
+        calibrations = self.get_calibrations()
+        return sum(calibrations)
+    
+    def get_calibrations(self):
+        digit_lines = self.get_digits_per_line()
+        return [digit_line[0] * 10 + digit_line[-1] for digit_line in digit_lines]
+    
+    def get_digits_per_line(self):
+        return [
+            [
+                self.parse(line, i)
+                for i, _ in enumerate(line)
+                if self.parse(line, i) != -1
+            ]
+            for line in self.lines
+        ]
+    
+    #ABSTRACT METHOD
+    #Returns digit corresponding to char at i in line
+    #Returns -1 if char does not correspond to digit
+    @abstractmethod
+    def parse(self, line, i):
+        pass
+
+    def get_lines(self, filename):
+        with open(filename, "r") as f:
+            self.lines = f.readlines()
