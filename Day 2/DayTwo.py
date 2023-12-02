3c9e8c609671c72a6b0b21b3db57687905f0eb7b
bweber0223 <36210094+bweber0223@users.noreply.github.com>
2023-12-02 11:21:57 -0500

    Create DayTwo.py

---
+++ Day 2/DayTwo.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+
+class DayTwo:
+    def main(self):
+        filename = input("Input file: ")
+        lines = self.get_lines(filename)
+        print(f"Answer: {self.process(lines)}")
+
+    def get_lines(self, filename):
+        with open(filename, "r") as f:
+            return f.readlines()
+        
+    @abstractmethod
+    def process(self, lines):
+        pass
